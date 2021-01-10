a62da05abaaf24ebf42fc1aeec31871f0fe1f1eb
Prasan Singh <66557300+Prasan26@users.noreply.github.com>
2021-01-11 01:23:50 +0530

    pattern

---
+++ Pattern_Problems/Palindromic_pattern.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int i,j,r;
+    cout<<"enter value for r :\n";
+    cin>>r;
+
+    for(i=1;i<=r;i++)
+    {
+        int k=i;
+        for(j=1; j<=(r-i); j++) {
+            cout<<" ";
+        }
+        for( ;j<=r; j++)
+        {
+            cout<<k<<" ";
+            k--;
+        }
+        k=1;
+        for( ;j<(r+i); j++)
+        {
+            k++;
+            cout<<k<<" ";
+        }
+        for( ;j<=(2*r-1); j++)
+        {
+            cout<<" ";
+        }
+        cout<<endl;
+
+    }
+    return 0;
+}
