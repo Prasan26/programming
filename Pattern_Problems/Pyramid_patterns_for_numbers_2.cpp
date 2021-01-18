4d58635031ba5eb11830ef1245837f62ca161a08
Prasan Singh <66557300+Prasan26@users.noreply.github.com>
2021-01-18 13:43:56 +0530

    Pyramid2

---
+++ Pattern_Problems/Pyramid_patterns_for_numbers_2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+
+    int n;
+    cout<<"Enter the value for n: \n";
+    cin>>n;
+
+    int space = n-1;
+    for(int i=1; i<=n; i++) {
+        for(int j=1; j<= space ; j++) {
+            cout<< " ";
+        }
+        space--;
+        for (int j=1; j<=i; j++) {
+            cout<<j<<" ";
+        } 
+        cout<< "\n";
+    }
+    return 0;
+}
